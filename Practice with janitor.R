6bb910ace2b5a7b436335e317b4edcf7652d51c7
Elke van Daal <elkevdaal8@gmail.com>
2023-05-31 20:43:03 +0200

    Committing useful janitor functions

---
+++ Practice with janitor.R
@@ -0,0 +1,27 @@
+# practice with janitor
+# practice with 
+
+# useful links
+# https://github.com/sfirke/janitor
+
+# install packages
+install.packages('janitor')
+install.packages('knitr')
+install.packages('lubridate')
+
+# load packages
+library(janitor)
+library(tidyverse)
+library(knitr)
+library(lubridate)
+
+# useful functions
+clean_names() #remove weird characters is columnnames
+get_dupes() #identify duplicates
+tabyl() #create tabulated df of selected variables
+adorn_() #summarize results of tabyl df
+remove_empty(c("rows", "cols")) #remove empty rows and columns
+remove_constant(na.rm = TRUE, quiet = FALSE) #remove columns with all identical values and print wich are removed (quiet)
+mutate(date_column = convert_to_date(date_column, # handle the mixed-format dates to one 
+                                   character_fun = mdy)) #lubridate notation
+
